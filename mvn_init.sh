14b64ec01039b2cb397a33b19884e521cebf6354
Jerry Yang <efjerryyang@outlook.com>
2024-07-10 16:04:08 -0700

    chore: add maven init and protos generated code

---
+++ mvn_init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mvn archetype:generate -DgroupId=com.example -DartifactId=tutorial -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+
+# use from parent directory, will generate project `tutorial`
